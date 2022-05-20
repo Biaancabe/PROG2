305cdd6c50f2db54a47ef0c9743cedde7e95f310
bianc <100831163+Biaancabe@users.noreply.github.com>
2022-05-20 13:50:43 +0200

    falls kein Datum eingegeben, richtiger Graf Relation Grafik versucht

--- libs/vis.py
+++ libs/vis.py
@@ -2,6 +2,9 @@ from plotly import express as px
 from plotly.offline import plot
 
 
+# gibt uns ein div
+# durch das Einsetzten von x, y kann man die Funktion auch für andere Werte als Anzahl & Getränke verwenden
+
 def viz1(x, y):
     # x-Achse = x | y-Achse = y (x & y werden bei def evaluation definiert)
     fig = px.bar(x=x, y=y)
@@ -9,8 +12,8 @@ def viz1(x, y):
     return div
 
 
-def viz2(x, y):
-    dr = px.data.gapminder().query("getraenk=='getraenk'")
-    fig = px.line(df, x=x, y=y, colour="getraenk")
-    div = plot(fig, output_type="div")
+def viz2(x1, x2, y1, y2):
+    fig1 = px.line(x=x1, y=y1, markers=True)
+    fig2 = px.line(x=x2, y=y2, markers=True)
+    div = plot(fig1, output_type="div")
     return div
